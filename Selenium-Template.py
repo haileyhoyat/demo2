b90a77bf0b86cfd1e00caf814ec7d043926dd898
Hails <whatuphails@yahoo.com>
2024-03-31 14:37:49 -0400

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -35,6 +35,6 @@ driver = webdriver.Chrome(options = chrome_options)
 
 driver.get('http://github.com')
 print(driver.title)
-with open('./GitHub_Action_Results.txt', 'w') as f:
-    f.write(f"This was written with a GitHub action {driver.title}")
+# with open('./GitHub_Action_Results.txt', 'w') as f:
+#    f.write(f"This was written with a GitHub action {driver.title}")
 
